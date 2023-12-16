d9d6df3bfb68d5830d0bae9bacf486f37442fe74
enricovittorini <enrico.vittorini@outlook.com>
2023-12-16 18:06:35 +0100

    EIT Actual long description and fix

--- client/app/eitActual.js
+++ client/app/eitActual.js
@@ -1,14 +1,44 @@
 'use client'
 import { useState, useEffect } from 'react';
 
+function ShortEventDescription({ id, event }) {
 
+    return (
+        <li key={"short_desc_" + id}>
+            <details>
+                <summary>Short Event Description</summary>
+                <ul>
+                    <li key="event_language" className='prop' >Language: {event.short_description.language_code}</li>
+                    <li key="event_name" className='prop' >Event Name: {event.short_description.name}</li>
+                    <li key="event_short" className='prop' >Description: {event.short_description.description}</li>
+                </ul>
+            </details>
+        </li>
+    )
+}
+
+function LongEventDescription({ id, event }) {
+
+    return (
+        <li key={"long_desc_" + id}>
+            <details>
+                <summary>Extended Event Description</summary>
+                <ul>
+                    <li key="event_name" className='prop' >Language: {event.long_description.language_code}</li>
+                    <li key="event_short" className='prop' >Description: {event.long_description.description}</li>
+                </ul>
+            </details>
+        </li>
+
+    )
+}
 
 
 function Events({ data }) {
 
 
     return data.map(k => {
-        
+
         const eventId = k.properties.event_id;
         const running = k.properties.running_status;
         const caMode = k.properties.ca_mode.toString();
@@ -21,20 +51,12 @@ function Events({ data }) {
                 <details>
                     <summary>Event Id: {eventId}</summary>
                     <ul>
-                        <li key="startTime" className='prop'>start time: {startTime}</li>
+                        <li key="startTime" className='prop'>start_time: {startTime}</li>
                         <li key="duration" className='prop'>duration: {duration}</li>
                         <li key="ca" className='prop'>ca_mode: {caMode}</li>
                         <li key="running" className='prop'>running: {running}</li>
-                        <li key={"short_desc_" + eventId}>
-                            <details>
-                                <summary>Short Event Description</summary>
-                                <ul>
-                                    <li key="event_name" className='prop' >Event Name: {k.short_description.name}</li>
-                                    <li key="event_short" className='prop' >Event: Description: {k.short_description.description}</li>
-                                </ul>
-                            </details>
-                        </li>
-
+                        <ShortEventDescription id={eventId} event={k} />
+                        <LongEventDescription id={eventId} event={k} />
                     </ul>
                 </details>
             </li>
@@ -45,38 +67,27 @@ function Events({ data }) {
 
 
 function Properties({ data }) {
-    const serviceId = data.service_id;
-    const events = data["#nodes"];
-
 
     return (
-        <>
-            {/* <Events data={events} />*/}
-            {Object.keys(data).map(k => {
-
-
-                return (
-                    <li key={`eit_prop_${serviceId}_${k}`} className="prop">
-                        {`${k}: ${data[k]?.toString()}`}
-                    </li>
-                )
-
-            })}
-        </>
-
+        Object.keys(data).map(k => {
+            return (
+                <li key={`eit_prop_${k}`} className="prop">
+                     {`${k}: ${data[k] ?? 'undefined'}`}
+                </li>
+            )
+        })
     )
-
 }
 
-function EitActual({ data, pids}) {
+function EitActual({ data, pids }) {
     const [eit, setEit] = useState();
     const [bitrate, setBitrate] = useState(0);
-    
+
 
     useEffect(() => {
 
         setEit(data);
-        setBitrate(pids?.find(k=> k.id === 18).bitrate);
+        setBitrate(pids?.find(k => k.id === 18).bitrate || 0);
     }, [data, pids])
 
     /*useEffect(() => {
@@ -98,7 +109,7 @@ function EitActual({ data, pids}) {
                                     <ul>
 
                                         {
-                                            data.eitpf.map(k => (
+                                            data.eitpf?.map(k => (
                                                 <li key={"eit_actual_" + k?.properties.service_id}>
                                                     <details>
                                                         <summary> Service {k?.properties.service_id}</summary>
